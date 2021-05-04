9d1edb8cd4eb468963fdc49e351194eaa588df3f
bentaniamin1 <amin.bentani@hetic.net>
2021-05-04 12:05:52 +0200

    player class

---
+++ player2.py
@@ -0,0 +1,13 @@
+from caractere import Caractere
+
+
+class Player2(Caractere):
+    def __init__(self, firstname, name, classe_sociale, age, sexe):
+        super().__init__(firstname, name, classe_sociale, age, sexe)
+
+        self.list_cartes = [1, 2, 3]
+        self.list_boutdephrase = [1, 2, 3]
+        self.jauge_de_confiance = 0
+
+    def cc(self):
+        print(32345673456)
